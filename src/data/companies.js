e1d416a76df9576387cab9913a85feac88bbb4a8
Aniruddh1408 <aniruddhachars@gmail.com>
2025-12-27 23:03:44 +0530

    Added companies

--- src/data/companies.js
+++ src/data/companies.js
@@ -34,23 +34,70 @@ const companies = [
       }
     ]
   },
-  {
-    name: "FinCorp Solutions",
-    accounts: [
-      {
-        name: "Main Account",
-        balance: 16000,
-        transactions: [
-          {
-            date: "19/03/2024 11:33 PM",
-            credit: 16000,
-            utr: "CMS3938564916",
-            account: "ACOCF7RRUY407QHU"
-          }
-        ]
-      }
-    ]
-  }
+{
+  name: "FinCorp Solutions",
+  accounts: [
+    {
+      name: "Account 2",
+      balance: 16000,
+      transactions: [
+        {
+          date: "19/03/2024 11:33 PM",
+          credit: 16000,
+          utr: "CMS3938564916",
+          account: "ACOCF7RRUY407QHU"
+        }
+      ]
+    },
+    {
+      name: "Account 2",
+      balance: 24500,
+      transactions: [
+        {
+          date: "21/03/2024 10:12 AM",
+          credit: 8500,
+          utr: "CMS4455123456",
+          account: "ACT999VM9RR11AA"
+        }
+      ]
+    }
+  ]
+},
+{
+  name: "PaySecure Systems",
+  accounts: [
+    {
+      name: "Account 1",
+      balance: 45200,
+      transactions: [
+        {
+          date: "02/04/2024 09:45 AM",
+          credit: 12000,
+          utr: "CMS7788123456",
+          account: "ACOCF9PS1234567"
+        },
+        {
+          date: "05/04/2024 04:30 PM",
+          credit: 8000,
+          utr: "CMS7788567890",
+          account: "UPI-PS-8899"
+        }
+      ]
+    },
+    {
+      name: "Account 2",
+      balance: 97500,
+      transactions: [
+        {
+          date: "10/04/2024 11:15 AM",
+          credit: 25000,
+          utr: "CMS8899123400",
+          account: "ACTPSVM999001"
+        }
+      ]
+    }
+  ]
+}
 ];
 
 export default companies;
